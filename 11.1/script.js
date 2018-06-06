b9eab9beed6a9241b882f8f75bc9052f67653dbd
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-06-06 10:42:20 +0200

    Fix prototype placement and invoke printInfo()

--- 11.1/script.js
+++ 11.1/script.js
@@ -2,10 +2,10 @@ function Phone(brand, price, color){
   this.brand = brand;
   this.price = price;
   this.color = color;
+};
 
   Phone.prototype.printInfo = function(){
     console.log('This phone brand is ' + this.brand + '. It costs ' + this.price + '  and color is: ' + this.color + '.');
-  };
 
   Phone.prototype.turnOff = function(){
     alert('Type "yes" to turn off your phone');
@@ -30,3 +30,5 @@ function Phone(brand, price, color){
 var samsung = new Phone('Samsung Galaxy S6', 1000, 'graphite');
 var iphone = new Phone('Iphone 6s', 1200, 'pearl white');
 var onePlus = new Phone('One Plus One', 800, 'saphire');
+
+samsung.printInfo();
